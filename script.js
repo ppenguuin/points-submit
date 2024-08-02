9f9830efb732eb409ca705759c3f438ed15606bf
ppenguuin <kennethlundq@gmail.com>
2024-08-01 19:24:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function checkPoints() {
         return;
     }
 
-    const sheetUrl = 'https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbxdHxYmWuHM6-3NTLWQQbnJwbfMS2JZIVx7dTdtzcjWdu2rbv88XlpJr0w3C5M0nUGRZg/exec';
+    const sheetUrl = 'https://script.google.com/macros/s/AKfycbxdHxYmWuHM6-3NTLWQQbnJwbfMS2JZIVx7dTdtzcjWdu2rbv88XlpJr0w3C5M0nUGRZg/exec';
 
     try {
         const response = await fetch(sheetUrl);
